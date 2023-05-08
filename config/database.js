24b2e0bd7f3139465db72d47766ab060fcb025e6
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-08 16:37:00 -0400

    wip: install and config mongoose

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL);
+
+//connection is a property within mongoose that gives us info specific to our connection
+const db = mongoose.connection; 
+
+db.on('connected', () => {
+    console.log(`connected to MongoDB using ${db.name} at ${db.host}:${db.port}`);
+})
