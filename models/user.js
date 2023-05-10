cbd064c6f53b675bede9c72f19c99a7a6eaf8978
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-10 16:42:51 -0400

    add userschema bcrypt middleware

--- models/user.js
+++ models/user.js
@@ -23,12 +23,23 @@ const userSchema = new Schema({
     timestamps: true,
     //toJSON allows us to define a hook function that is invoked when the key happens, ie. when data is converted to json
     //doc is version of mongoose data document tht cannot be mutated
-    //doc is version of mongoose data document tht can be mutated, then handed back to model to create JWT from it
-    toJSON: function(doc, ret) {
-        delete ret.password;
-        return ret;
+    //ret(representation) is version of mongoose data document that can be mutated, then hand it back to model to create JWT from it
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.password;
+            return ret;
+        }
     }
 });
+
+//Schema middleware
+//'save' event happens when user doc is created or modified
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next(); //breaks us out, doesn't hash password again
+    //hash password
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+    return next(); //return modified version of the doc with the hashed/salted password added to the database
+})
 
 module.exports =mongoose.model('User', userSchema);
 
