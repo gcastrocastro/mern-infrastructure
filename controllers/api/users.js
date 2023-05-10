a891d5939173f08e5c77dd62ae9ccc9c66ed2e4c
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-10 18:47:49 -0400

    wip: createsJWT

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,11 +1,28 @@
+const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     create
 }
 
-function create(req, res){
-    res.json({
-        name: req.body.name,
-        email: req.body.email
-    })
+async function create(req, res){
+    try {
+        //1. create user
+        const user = await User.create(req.body);
+        //2. create the jwt by passing in the user info for the jwt payload
+        //createJWT() is when the user doc is converted to json and therefore the password is removed in this step
+        const token = createJWT(user); 
+        //3. send the new jwt to the client using res.json
+        res.json(token);
+    } catch (error) {
+        // if error, we'll send the error to the client
+        res.status(400).json(error);
+    }
+}
+
+//helper function, defined outside bc we want it to be reuseable
+function createJWT(user) {
+    //jwt.sign() creates token and cryptographically 'sign' it using secret, to validate it was created by server and not expired
+    //needs 3 things, user object to create our JWT payload, secret we create, and an optional settings object
+    return jwt.sign({user}, process.env.SECRET, {expiresIn: '24h'})
 }
