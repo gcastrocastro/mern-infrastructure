0a7cbe608e256de930c509f63fe546bf7558a2f4
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-11 14:39:52 -0400

    getUser() and pass to app.js

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -5,14 +5,15 @@ import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import {Route, Routes} from 'react-router-dom';
 import NavBar from '../../Components/NavBar/NavBar';
+import {getUser} from '../../utilities/users-service';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
   return (
     <main className="App">
       { user ?
         <>
-          <NavBar/>
+          <NavBar user={user}/>
           <Routes>
             {/* {Route Components Go Here} */}
             <Route path="/orders/new" element={<NewOrderPage />} />
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -4,6 +4,28 @@ export async function signUp(userData) {
     const token = await userAPI.signUp(userData);
     
     localStorage.setItem('token', token);
-    
+
     return token;
+}
+
+export function getToken() {
+    //attempt to get token from localstorage
+    const token = localStorage.getItem('token');
+    //this can be null when user hasn't logged in before 
+    if (!token) return null;
+    // if token is retrieved, 
+        //decode the payload from token so we can check if it's still valid (or expired)
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    if (payload.exp < Date.now() / 1000){
+        //if not valid, we remove the token
+        localStorage.removeItem('token');
+        return null;
+    } 
+    // else return token
+    return token;
+}
+
+export function getUser() {
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
